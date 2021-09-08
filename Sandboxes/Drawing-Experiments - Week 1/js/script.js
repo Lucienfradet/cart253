802b1e6eecc23cc0bd5f5240473e629c53d66a9b
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-08 17:51:06 -0400

    Edit colors and fiddled with things

--- Sandboxes/Drawing-Experiments - Week 1/js/script.js
+++ Sandboxes/Drawing-Experiments - Week 1/js/script.js
@@ -20,7 +20,32 @@ function preload() {
 Description of setup
 */
 function setup() {
-  background(255, 0, 0);
+  /**
+  createCanvas(500, 500);
+  background(127, 255, 127);
+  rectMode(CENTER);
+  stroke(277,55,55);
+  fill(240,230,200);
+  rect(250, 250, 300, 300);
+  fill(20,115,80);
+  ellipse(200, 200, 50, 50);
+  ellipse(300, 200, 50, 50);
+  fill(240,25,0);
+  rect(250, 200, 25, 200);
+  noFill();
+  arc(250, 300, 200, 100, 0, PI / 2);
+  */
+  createCanvas(500, 500);
+  background(127, 255, 127);
+  rectMode(CENTER);
+  //noStroke();
+  fill("#abcdef");
+  rect(250, 250, 300, 300);
+  ellipse(200, 200, 50, 50);
+  ellipse(300, 200, 50, 50);
+  rect(250, 200, 25, 200);
+  stroke(0);
+  arc(250, 300, 200, 100, 0, PI / 2);
 }
 
 
