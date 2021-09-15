17c1658386cb9faaf43b51935441e9a08f7e4b76
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-15 15:51:35 -0400

    Variables Experiments (Instead of the Mona Lisa thing lol)

---
+++ Sandboxes/Variables-Experiments - Week 2/js/script.js
@@ -0,0 +1,33 @@
+/**
+Drawing the Mona Lisa
+Lucien Cusson-Fradet
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(240, 350);
+  background(144, 191, 122);
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
