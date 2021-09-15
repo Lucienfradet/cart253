61abbc6f9fb844e8ea55e082ca04b266ff9a7d11
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-15 17:57:35 -0400

    Practiced using variables, objects, map and constrain functions!

--- Sandboxes/Variables-Experiments - Week 2/js/script.js
+++ Sandboxes/Variables-Experiments - Week 2/js/script.js
@@ -1,11 +1,19 @@
 /**
-Drawing the Mona Lisa
+Experimenting with variables
 Lucien Cusson-Fradet
 
 */
 
 "use strict";
 
+let backGroundShade = 0;
+let fillColor
+let circle = {
+  x: 0,
+  y: 250,
+  size: 200,
+  speed: 2
+}
 
 /**
 Description of preload
@@ -19,9 +27,8 @@ function preload() {
 Description of setup
 */
 function setup() {
-  createCanvas(240, 350);
-  background(144, 191, 122);
-
+  createCanvas(500, 500);
+  circle.x = 0 - circle.size/2;
 }
 
 
@@ -30,4 +37,23 @@ Description of draw()
 */
 function draw() {
 
+  background(backGroundShade);
+  fillColor = map(mouseX, 0, width, 0, 255);
+
+  fill(fillColor);
+  ellipse(circle.x, circle.y, circle.size);
+  circle.x += circle.speed;
+
+  backGroundShade += 1;
+
+  if (backGroundShade == 300) {
+    backGroundShade = 0;
+  }
+  if (circle.x >= width + circle.size/2) {
+    circle.x = 0 - circle.size/2;
+    circle.speed = random(1, 5);
+  }
+
+console.log(`NEWWAY circle.x: ${circle.speed}`);
+console.log("circle.x: " + circle.x);
 }
