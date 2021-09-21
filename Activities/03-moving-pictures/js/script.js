0810f2cd71e5857a2c360916d78e96f2f95641da
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-21 09:43:31 -0400

    Author name and title

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -1,13 +1,27 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Moving picture activity!
+Lucien Cusson-Fradet
 */
 
 "use strict";
 
+let circleRight = {
+  x:width,
+  y:height/2,
+  size:15,
+  speed:2,
+  alpha:200
+}
+
+let circleLeft = {
+  x:0,
+  y:height/2,
+  size:40,
+  speed:2,
+  alpha:80
+}
+
+let backgroundColor = 0;
 
 /**
 Description of preload
@@ -21,7 +35,11 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(500, 500);
 
+  //Place the two circles properly
+  circleRight.x =+ circleRight.size/2;
+  circleLeft.x =- circleLeft.size/2;
 }
 
 
@@ -29,5 +47,14 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(backgroundColor,0,0);
+  backgroundColor += 0.1;
+
+  noStroke();
+
+  fill(0,0,0,circleRight.alpha);
+  ellipse(circelRight.x, cricleRight.y, circelRight.size);
+  fill(0,0,0,circleLeft.alpha);
+  ellipse(circleLeft.x, circleLeft.y, circleLeft.size);
 
 }
