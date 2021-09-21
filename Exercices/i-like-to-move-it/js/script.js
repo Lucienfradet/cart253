c9a15e9a27773bbcb91a113a3b9455472ae3dbd1
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-21 10:48:29 -0400

    E1: started exercise 1

---
+++ Exercices/i-like-to-move-it/js/script.js
@@ -0,0 +1,30 @@
+/**
+Exercice 1 - I like to move it!
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
