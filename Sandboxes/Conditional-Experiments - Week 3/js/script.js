6550834f3476873a712afec261893fdf3e6751ef
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-27 19:53:04 -0400

    Week 3 - Testing things out with conditionnals!

---
+++ Sandboxes/Conditional-Experiments - Week 3/js/script.js
@@ -0,0 +1,30 @@
+/**
+Testing things out with conditionnals!
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
