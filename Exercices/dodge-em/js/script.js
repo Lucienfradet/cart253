7fe281406e5ba6ad7652fbc5033f549f6011d924
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-28 13:58:54 -0400

    Ex2: Started the exercice dodge 'em!

---
+++ Exercices/dodge-em/js/script.js
@@ -0,0 +1,30 @@
+/**
+Exercice 2: Dodge 'em!
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
