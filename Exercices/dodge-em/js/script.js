5049e7c37caa5a29411697a78eff826812f61659
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-28 15:04:04 -0400

    Ex2: added a polygone and some lighting contrtolled by the mouse

--- Exercices/dodge-em/js/script.js
+++ Exercices/dodge-em/js/script.js
@@ -4,7 +4,15 @@ Lucien Cusson-Fradet
 */
 
 "use strict";
+let canvasWidth = 500;
+let canvasHeight = 800;
 
+//redGem object
+let redGem = {
+  detailX: 7,
+  detailY: 4,
+
+}
 
 /**
 Description of preload
@@ -18,7 +26,9 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  smooth();
+  createCanvas(canvasWidth, canvasHeight, WEBGL);
+  background(0);
 }
 
 
@@ -26,5 +36,15 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  push();
+  scale(0.92,1);
+  pointLight(250, 250, 250, mouseX-width/2, mouseY-height/2, 500);
+  ambientLight(75);
+  rotateY(millis() / 1000);
+  fill(240,150,150);
+  noStroke();
+  sphere(50, redGem.detailX, redGem.detailY);
+  pop();
 }
