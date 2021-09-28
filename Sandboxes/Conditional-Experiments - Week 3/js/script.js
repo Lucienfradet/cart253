0b21719f8eb493c47afb5e1b5a9fc47299ca96d4
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-27 21:09:43 -0400

    Introduction to Conditionnals,  the different relational operatorsif statement and different

--- Sandboxes/Conditional-Experiments - Week 3/js/script.js
+++ Sandboxes/Conditional-Experiments - Week 3/js/script.js
@@ -5,20 +5,20 @@ Lucien Cusson-Fradet
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
+let backgroundShade = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 5,
+  fill: 255
 }
-
 
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(500,500);
 }
 
 
@@ -26,5 +26,26 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(backgroundShade);
+  backgroundShade += 0.5;
 
+  if (backgroundShade === 255) {
+    backgroundShade = 0;
+  }
+
+  if (circle.x > width + circle.size/2) {
+    circle.speed = -circle.speed;
+  }
+  if (circle.x < 0 - circle.size/2) {
+    circle.speed = -circle.speed;
+  }
+
+  fill(circle.fill);
+
+  if (mouseX < width/2) {
+    fill(255, 0, 0);
+  }
+  
+  circle.x = circle.x + circle.speed;
+  ellipse(circle.x, circle.y, circle.size);
 }
