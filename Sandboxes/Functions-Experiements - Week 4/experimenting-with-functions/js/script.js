ec96a29139d64db1bc8ace13e5712bb289f3481d
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-09-30 13:18:23 -0400

    Started the Functions learning material!

---
+++ Sandboxes/Functions-Experiements - Week 4/experimenting-with-functions/js/script.js
@@ -0,0 +1,29 @@
+/**
+Experimenting and learning Functions!
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
