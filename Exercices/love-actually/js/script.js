79ee259dd3c1e00a1c354a598eedf2b0b8494031
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-10-04 21:27:31 -0400

    Ex3: Started Exercice love-actually

---
+++ Exercices/love-actually/js/script.js
@@ -0,0 +1,29 @@
+/**
+Le grand Amour!
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
