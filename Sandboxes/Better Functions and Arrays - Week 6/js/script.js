f5bc8a136500aff788e3c720cbdfd8698b8fc279
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-10-19 17:59:52 -0400

    Update script.js

--- Sandboxes/Better Functions and Arrays - Week 6/js/script.js
+++ Sandboxes/Better Functions and Arrays - Week 6/js/script.js
@@ -1,74 +1,65 @@
 "use strict";
 
-// Our fish
-let fishes = [];
-const FISH_NUM = 10;
+let circle = {
+  x: 0,
+  y: 0,
+  size: 100,
+  trail: [],
+  maxTrail: 10 // NEW! Maximum number of positions in the trail array
+}
 
+let rates = [1.5, 1.75, 2.25, 2.5, 2.75, 3];
+let barkSFX;
+
+function preload() {
+  barkSFX = loadSound(`assets/sounds/bark.wav`);
+}
+
+// setup() the canvas ready
 function setup() {
   createCanvas(600, 600);
-
-  // Create four fish, positioned randomly
-  for (let i = 0; i < FISH_NUM; i++) {
-    let fish = createFish(random(0, width), random(0, height));
-    fishes.push(fish);
-  }
 }
 
-// createFish(x,y)
-// Creates a new JavaScript Object describing a fish and returns it
-function createFish(x, y) {
-  let fish = {
-    x: x,
-    y: y,
-    size: 50,
-    vx: 0,
-    vy: 0,
-    speed: 2
-  };
-  return fish;
-}
-
-// draw()
-// Moves and displays our fish
+// draw() draws a circle with a trails
 function draw() {
   background(0);
 
-  for (let i = 0; i < fishes.length; i++) {
-    moveFish(fishes[i]);
-    displayFish(fishes[i]);
-  }
-}
-
-// moveFish(fish)
-// Chooses whether the provided fish changes direction and moves it
-function moveFish(fish) {
-  // Choose whether to change direction
-  let change = random(0, 1);
-  if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
+  // Use a for loop to go through each element in the circle's trail array in order
+  for (let i = 0; i < circle.trail.length; i++) {
+    // Get the element at the index indicated by i (0, then 1, then 2, etc.)
+    let element = circle.trail[i];
+    // Draw an ellipse the same size as the circle at that position
+    ellipse(element.x, element.y, circle.size);
   }
 
-  // Move the fish
-  fish.x = fish.x + fish.vx;
-  fish.y = fish.y + fish.vy;
+  // Move the circle to the mouse position
+  circle.x = mouseX;
+  circle.y = mouseY;
 
-  // Constrain the fish to the canvas
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
-}
+  // Draw the circle
+  ellipse(circle.x, circle.y, circle.size);
 
-// displayFish(fish)
-// Displays the provided fish on the canvas
-function displayFish(fish) {
-  push();
-  fill(200, 100, 100);
-  noStroke();
-  ellipse(fish.x, fish.y, fish.size);
-  pop();
+  // Create a new position object that stores where the circle is now
+  // which we can add to the trail to trace the path of the circle
+  let newTrailPosition = {
+    x: circle.x,
+    y: circle.y
+  };
+  // Add the position to the circle's trail array
+  circle.trail.push(newTrailPosition);
+
+  // NEW! Check if the trail's length has exceeded the maximum
+  if (circle.trail.length > circle.maxTrail) {
+    // If it has, remove the oldest element (the one at the START of the array)
+    circle.trail.shift();
+  }
 }
 
 function mousePressed() {
-  let newFish = createFish(mouseX, mouseY);
-  fishes.push(newFish);
+  // Choose a random rate from the array
+  let barkRate = random(rates);
+  // Set the barking sound effect to that rate
+  barkSFX.rate(barkRate);
+  // Play the barking sound effect
+  barkSFX.play();
 }
