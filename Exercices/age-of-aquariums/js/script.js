9ced5a47c7e30667f88f41281a0f13057de503d2
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-10-21 11:13:48 -0400

    Ex4: Started age of aquarium exercice!

---
+++ Exercices/age-of-aquariums/js/script.js
@@ -0,0 +1,29 @@
+/**
+3D aquarium?
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
