a6a0865b24709e5b4b6673d7b65f7d0f4fcd1803
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-10-21 14:36:39 -0400

    E4: constrained the particles to a box to make it look like an aquarium, Added another array that diplays a trail but it's slowing the animation considerably...

--- Exercices/age-of-aquariums/js/script.js
+++ Exercices/age-of-aquariums/js/script.js
@@ -32,6 +32,8 @@ function createParticle() {
     positionV: createVector(0, 0, 0),
     speedV: p5.Vector.random3D(),
     accelerationV: createVector(),
+    trail: [],
+    maxTrail: 0
   }
   particle.speedV.setMag(random(1, 2));
 
@@ -44,6 +46,12 @@ Description of setup
 function setup() {
   createCanvas(canvasWidth, canvasHeight ,WEBGL);
   background(0);
+
+  //var fps = 60;
+
+  //var capturer = new CCapture({ format: 'png', framerate: fps});
+
+  //capturer.start();
 }
 
 /**
@@ -51,13 +59,16 @@ Description of draw()
 */
 function draw() {
   background(0);
+  rotateY(radians(rotationAngle));
+  rotationAngle += 0.5;
+
   for (let i = 0; i < particles.length; i++) {
     moveParticle(particles[i]);
     displayParticle(particles[i]);
   }
 
-  rotate(radians(rotationAngle));
-  rotationAngle += 5;
+  //capturer.capture(document.getElementById('defaultCanvas0'));
+  //saveFrames('out', 'png', 1, 25, data => {print(data);});
 }
 
 function moveParticle(particle) {
@@ -67,16 +78,36 @@ function moveParticle(particle) {
     particle.speedV.setMag(random(1, 2));
   }
 
-  particle.positionV.limit(200);
-  //constrain(particle.positionV.x, -100, 100);
   particle.positionV.add(particle.speedV);
 
+  let currentVx = constrain(particle.positionV.x, -150, 150);
+  let currentVy = constrain(particle.positionV.y, -100, 100);
+  let currentVz = constrain(particle.positionV.z, -100, 100);
+  particle.positionV = createVector(currentVx, currentVy, currentVz);
+
+  let newTrailPosition = createVector(particle.positionV.x, particle.positionV.y, particle.positionV.z);
+  particle.trail.push(newTrailPosition);
+
+  if (particle.trail.length > particle.maxTrail) {
+    particle.trail.shift();
+  }
 }
 
 function displayParticle(particle) {
+  push();
+  for (let i = 0; i < particle.trail.length; i++) {
+    let trail = particle.trail[i];
+    strokeWeight(3);
+    let alpha = map(i, 0, particle.trail.length, 0, 255);
+    stroke(alpha);
+    point(trail.x, trail.y, trail.z);
+  }
+  pop();
+
   push();
   stroke(255);
   strokeWeight(3);
   point(particle.positionV.x, particle.positionV.y, particle.positionV.z);
   pop();
+
 }
