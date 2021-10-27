0ab7490a7e0d4efdf746cedff696e27a09551987
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-10-27 00:59:12 -0400

    Activities with "Class"

--- Sandboxes/Object Oriented Programming - Week 7/js/Flower.js
+++ Sandboxes/Object Oriented Programming - Week 7/js/Flower.js
@@ -1,28 +1,58 @@
 class Flower {
-  constructor() { //set's up this's properties
-      // Position and size information
-      this.x = random(0, width);
-      this.y = random(0, height);
-      this.size = 50;
-      this.stemLength = 75;
-      this.stemThickness = 10;
-      this.petalThickness = 10;
-      // Color information
-      this.stemColor = {
-        r: 50,
-        g: 150,
-        b: 50
-      };
-      this.petalColor = {
-        r: 200,
-        g: 50,
-        b: 50
-      };
-      this.centreColor = {
-        r: 50,
-        g: 0,
-        b: 0
-      };
+  constructor({
+    x,
+    y,
+    size,
+    stemLength,
+    stemThickness,
+    petalThickness,
+    stemColor,
+    petalColor,
+    centreColor
+  }) {
+    //set's up this's properties
+    // Position and size information
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.stemLength = stemLength;
+    this.stemThickness = stemThickness;
+    this.petalThickness = petalThickness;
+    // Color information
+    this.stemColor = stemColor;
+    this.petalColor = petalColor;
+    this.centreColor = centreColor;
+    this.alive = true;
+  }
+
+  // shrink()
+  // Shrinks the flower
+  shrink() {
+   // Choose a random amount to shrink
+   let shrinkage = random(0, 0.1);
+   // Reduce the petal thickness (divide by 10 to make it less rapid)
+   this.petalThickness = this.petalThickness - shrinkage / 10;
+   // Reduce the centre of the flower
+   this.size = this.size - shrinkage;
+
+   // If any of the key properties reach 0 or less, the flower is dead
+   if (this.petalThickness <= 0 || this.size <= 0) {
+     this.alive = false;
+   }
+  }
+
+  // NEW! pollinate() handles the flower being pollinated (it grows)
+  pollinate() {
+   // Choose a random amount to grow
+   let growth = random(0, 0.5);
+   // Increase the petal thickness (divide by 10 to make it less rapid)
+   this.petalThickness = this.petalThickness + growth / 10;
+   // Increase the centre of the flower
+   this.size = this.size + growth;
+
+   // Constrain the elements
+   this.petalThickness = constrain(this.petalThickness, 0, this.maxPetalThickness);
+   this.size = constrain(this.size, 0, this.maxSize);
   }
 
   display() {
@@ -38,4 +68,17 @@ class Flower {
     ellipse(this.x, this.y, this.size);
     pop();
   }
+
+    mousePressed() {
+      // Calculate the distance between this flower and the mouse
+      let d = dist(this.x,this.y,mouseX,mouseY);
+      // Check if the distance is less than the head of the flower
+      if (d < this.size/2 + this.petalThickness) {
+        // If it is, this flower was clicked, so increase its stem length
+        this.stemLength = this.stemLength + 10;
+        // And also change its y position so it grows upward! (If we didn't do this
+        // the then stem would grow downward, which would look weird.)
+        this.y = this.y - 10;
+      }
+    }
 }
--- Sandboxes/Object Oriented Programming - Week 7/js/script.js
+++ Sandboxes/Object Oriented Programming - Week 7/js/script.js
@@ -13,13 +13,17 @@ let garden = {
   // An array to store the individual flowers
   flowers: [],
   // How many flowers in the garden
-  numFlowers: 20,
+  numFlowers: 200,
   // The color of the grass (background)
   grassColor: {
     r: 120,
     g: 180,
     b: 120
-  }
+  },
+  // An array to our the bees
+  bees: [],
+  // How many bees in the garden
+  numBees: 200,
 };
 
 /**
@@ -38,11 +42,48 @@ function setup() {
 
   // Create our flowers by counting up to the number of the flowers
   for (let i = 0; i < garden.numFlowers; i++) {
-    // Create a new flower
-    let flower = new Flower();
+    // Create the congig properties of the new flower
+    let config = {
+      x: random(0, width),
+      y: random(0, height),
+      size: random(50, 80),
+      stemLength: random(50, 100),
+      stemThickness: random(5, 15),
+      petalThickness: random(20, 25),
+      // Color information
+      stemColor: {
+        r: 50,
+        g: random(100, 200),
+        b: 50
+      },
+      petalColor: {
+        r: random(100, 255),
+        g: random(100, 255),
+        b: random(50, 150)
+      },
+      centreColor: {
+        r: random(50, 100),
+        g: 0,
+        b: 0
+      }
+    };
+
+    //creates the new Flower object using the config variables as arguments
+    let flower = new Flower(config);
     // Add the flower to the array of flowers
     garden.flowers.push(flower);
     }
+
+    // Create our bees by counting up to the number of bees
+  for (let i = 0; i < garden.numBees; i++) {
+    // Create variables for our arguments for clarity
+    let x = random(0, width);
+    let y = random(0, height);
+    // Create a new bee using the arguments
+    let bee = new Bee(x, y);
+    // Add the bee to the array of bees
+    garden.bees.push(bee);
+  }
 
   garden.flowers.sort(sortByY);
 }
@@ -62,5 +103,39 @@ function draw() {
   for (let i = 0; i < garden.flowers.length; i++) {
     let flower = garden.flowers[i];
     flower.display();
+    // Check if this flower is alive
+    if (flower.alive) {
+      // Update the flower by shrinking it and displaying it
+      flower.shrink();
+      flower.display();
+    }
+  }
+
+  for (let i = 0; i < garden.bees.length; i++) {
+    let bee = garden.bees[i];
+    // Check if this flower is alive
+    if (bee.alive) {
+      // Shrink and move the bee
+      bee.shrink();
+      bee.move();
+
+      // NEW! Go through the entire flower array and try to pollinate the flowers!
+      // Note that we use j in our for-loop here because we're already inside
+      // a for-loop using i!
+      for (let j = 0; j < garden.flowers.length; j++) {
+        let flower = garden.flowers[j];
+        bee.tryToPollinate(flower);
+      }
+
+      // Display the bee
+      bee.display();
+    }
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < garden.flowers.length; i++) {
+    let flower = garden.flowers[i];
+    flower.mousePressed();
   }
 }
