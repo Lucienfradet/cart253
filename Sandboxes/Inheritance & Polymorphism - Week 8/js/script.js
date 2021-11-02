da5dba60a31538becd3cfb3dd3b8be1bcd6d30b5
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-11-02 12:51:07 -0400

    Started Learning materials about inheritance and polymorphism

---
+++ Sandboxes/Inheritance & Polymorphism - Week 8/js/script.js
@@ -0,0 +1,29 @@
+/**
+Sandbox about Inheritance and Polymorphism
+Lucien Cusson-Fradet
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
