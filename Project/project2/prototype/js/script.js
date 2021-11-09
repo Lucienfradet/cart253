397cff1846ff1e4e921001134a2cce3093818fec
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-11-09 09:24:24 -0500

    P2 Prototype: Started Prototype for project 2!

---
+++ Project/project2/prototype/js/script.js
@@ -0,0 +1,44 @@
+/**
+Project 2 prototype
+Lucien Cusson-Fradet
+
+- Create a tunnel with single layer
+- Add perlin noise
+- Add an object that follows the tunnel edges based on a physics system
+  - Gravity
+  - Normal Force
+  - Angular momentum
+  - Friction
+- Add a "jump" event
+- Make the tunnel a tunnel (3D perlin noise)
+- Add a radar thing
+- Spawn objects and make them follow the tunnel
+- Collision with the main object
+- Implement dithering effect!?
+
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
