54d10aa25a7426ca61f660a4ae5bca5b94dff257
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-11-12 17:05:07 -0500

    Started p5.sound learning materials

---
+++ Sandboxes/Sound Library - Week 9/js/script.js
@@ -0,0 +1,28 @@
+/**
+Play with p5.sound Library!
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
