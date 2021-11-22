413655f0725308cb039bd53be134686e8aefed3d
Lucien C.Fradet <lucien.fradet@hotmail.com>
2021-11-22 08:45:22 -0500

    Update script.js

--- Project/project2/project2/js/script.js
+++ Project/project2/project2/js/script.js
@@ -28,6 +28,11 @@ OK? Add a radar thing
 
 "use strict";
 
+//module aliases
+let Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
 let canvasWidth = 700;
 let canvasHeight = 550;
 
@@ -75,13 +80,13 @@ function draw() {
   time = frameCount/60;
 
   //Deploys the tunnel after an amount of time
-  if (time > 5) {
-    for (let i = 0; i < tunnel.length; i++) {
-      tunnel[i].deploy();
-      radar.display();
-      radar.rotate();
-    }
-  }
+  // if (time > 5) {
+  //   for (let i = 0; i < tunnel.length; i++) {
+  //     tunnel[i].deploy();
+  //     radar.display();
+  //     radar.rotate();
+  //   }
+  // }
 
   // let r = random();
   // if (r < 0.1) {
